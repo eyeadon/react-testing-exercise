3aa7a49630ae4ca20663c329ef13bb539ada15fe
eyeadon <eyeadon40@gmail.com>
2025-01-19 17:14:19 -0500

    setup mock service worker

---
+++ tests/mocks/handlers.ts
@@ -0,0 +1,12 @@
+import { http, HttpResponse } from "msw";
+
+export const handlers = [
+  // request handler, response resolver
+  http.get("/categories", () => {
+    return HttpResponse.json([
+      { id: 1, name: "Electronics" },
+      { id: 2, name: "Beauty" },
+      { id: 3, name: "Gardening" },
+    ]);
+  }),
+];
